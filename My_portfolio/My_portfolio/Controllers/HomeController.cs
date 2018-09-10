93543cf3edb830309c32161c9ed55b32ba366414
Alex Groshev <alcasatel@gmail.com>
2018-09-10 10:49:42 +0300

    translated most english words to russian. Added some variables in controller

--- My_portfolio/My_portfolio/Controllers/HomeController.cs
+++ My_portfolio/My_portfolio/Controllers/HomeController.cs
@@ -10,13 +10,26 @@ namespace Metanit.Controllers
 {
     public class HomeController : Controller
     {
-        public string Name = "Грошев Александр";
-        public string Vocation = "";
+        public string name = "Грошев Александр";
+        public string vocation = "Junior программист C#";
+        public int age = 25;
+        public string email = "alcasatel@gmail.com";
+        public string adress = "Волгоград, центральный район";
+        public string phone = "8 995 429 36 69";
+        public string languages = "Русский родной, Английский свободный, латышский средний";
+        public string about = "<p>Euismod massa scelerisque suspendisse fermentum habitant vitae ullamcorper magna quam iaculis, tristique sapien taciti mollis interdum sagittis libero nunc inceptos tellus, hendrerit vel eleifend primis lectus quisque cubilia sed mauris.</p> ";
+
         // GET: Home
         public ActionResult Index()
         {
-            ViewBag.name = Name;
-            ViewBag.Vocation = Vocation;
+            ViewBag.name = name;
+            ViewBag.vocation = vocation;
+            ViewBag.age = age;
+            ViewBag.email = email;
+            ViewBag.adress = adress;
+            ViewBag.phone = phone;
+            ViewBag.lang = languages;
+            ViewBag.about = about;
             return View();
         }
 
