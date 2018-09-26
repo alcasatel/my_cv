17160586c8045de8d81e12d57706a80e339a6168
Alex Groshev <alcasatel@gmail.com>
2018-09-26 20:48:13 +0300

    Added login functions

---
+++ My_portfolio/My_portfolio/Controllers/AccountController.cs
@@ -0,0 +1,66 @@
+﻿using Metanit.Models;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Metanit.Controllers
+{
+    public class AccountController : Controller
+    {
+        public string Index()
+        {
+            string result = "Вы не авторизованы";
+            if (User.Identity.IsAuthenticated)
+            {
+                result = "Ваш логин: " + User.Identity.Name;
+            }
+            return result;
+        }
+
+        public ActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                ViewBag.Email = User.Identity.Name;
+                return View("Logoff");
+            }
+
+            return View("Login");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // поиск пользователя в бд
+                User user = null;
+                using (UserContext db = new UserContext())
+                {
+                    user = db.Users.FirstOrDefault(u => u.Email == model.Name && u.Password == model.Password);
+
+                }
+                if (user != null)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Name, true);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователя с таким логином и паролем нет");
+                }
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Logoff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account");
+        }
+
+        
+    }
+}
---
+++ My_portfolio/My_portfolio/Models/UserContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace Metanit.Models
+{
+    public class UserContext :DbContext
+    {
+        public UserContext() : base("DefaultConnection") { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
